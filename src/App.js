7c7fb68853b4ae773f32c58813288029fe7378bd
Muhammad Shakeel ur Rehman Tariq <70shakeel@gmail.com>
2020-06-09 22:03:26 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function Student({ name, grade, index, removeStudent }) {
+  return (
+    <div>
+      <p>
+        Student Name: {name}, Grade: {grade}
+      </p>
+
+      <div>
+        <button onClick={() => removeStudent(index)}>x</button>
+      </div>
+    </div>
+  );
+}
+
+function StudentForm({ addStudent }) {
+  const [value, setValue] = useState("");
+  const [grade, setGrade] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addStudent(value, grade);
+    setValue("");
+    setGrade("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        name="name"
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Enter Name of Student"
+      />
+      <input
+        type="text"
+        className="input"
+        name="grade"
+        value={grade}
+        onChange={(e) => setGrade(e.target.value)}
+        placeholder="Enter Grade of Student"
+      />
+      <button onClick={handleSubmit}>Add Student</button>
+    </form>
+  );
+}
 
 function App() {
+  const [student, setStudents] = useState([
+    {
+      name: "Alice",
+      grade: "A",
+    },
+    {
+      name: "Bob",
+      grade: "B",
+    },
+    {
+      name: "Charlie",
+      grade: "C",
+    },
+  ]);
+
+  const addStudent = (name, grade) => {
+    const newStudent = [...student, { name, grade }];
+    setStudents(newStudent);
+  };
+
+  const removeStudent = (index) => {
+    console.log(...student);
+    const newStudent = [...student];
+    newStudent.splice(index, 1);
+    console.log(newStudent);
+    setStudents(newStudent);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>
+        {student
+          .sort((a, b) => (a.grade > b.grade ? 1 : -1))
+          .map((student, index) => (
+            <Student
+              key={index}
+              index={index}
+              grade={student.grade}
+              name={student.name}
+              removeStudent={removeStudent}
+            />
+          ))}
+        <StudentForm addStudent={addStudent} />
+      </div>
     </div>
   );
 }
